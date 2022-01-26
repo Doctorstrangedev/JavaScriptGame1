d40c43070ec1e467dba0006e98ff5980a3299cd2
Daniel Portales <portalesbeltrandaniel@gmail.com>
2022-01-25 22:32:23 -0300

    funciones start game y create deck

--- asset/js/game.js
+++ asset/js/game.js
@@ -14,6 +14,9 @@
     const types = ['C', 'D', 'H', 'S'];
     const specials = ['A', 'J', 'Q', 'K'];
 
+    //Score de los jugadores
+    let scorePlayers = [];
+
     /*--- REFERENCIAS AL DOM  ---*/
 
     //botones de acciones
@@ -21,7 +24,85 @@
     const stopBtnTurn = document.querySelector("#btnStopTurn");
     const newBtnGame = document.querySelector("#btnNewGame");
 
-    //referencias al area del juego
+    //referencias al area de juego
+
+    const divCardPlayer = document.querySelectorAll('.divCards');
+    const scoreHtml = document.querySelectorAll('small');
+    /*FIN DE REFERENCIAS AL DOM */
+
+    //Funciones
+    //iniciar el juego
+    const startGame = (numPlayer = 2) => {
+
+        //cada nuevo juego se reinician los pts
+        console.log('HORA DE COMENZAR EL JUEGO');
+        //crear baraja
+        deckOfCards = createDeck();
+
+        //el puntaje de los jugadores
+        scorePlayers = [];
+        for (let i = 0; i < numPlayer; i++) {
+            scorePlayers.push(0);
+        }
+        console.log(scorePlayers);
+
+    };
+
+    //Crear la baraja
+    const createDeck = () => {
+        let deckOfCards = [];
+        for (let i = 2; i <= 10; i++) {
+            for (let type of types) {
+                deckOfCards.push(i + type)
+            }
+        }
+        for (let type of types) {
+            for (let special of specials) {
+                deckOfCards.push(special + type)
+            }
+        }
+        console.log(deckOfCards);
+        return _.shuffle(deckOfCards);
+    };
+
+    //Obrener una carta
+    const getOneCard = () => {
+
+    };
+
+    //Valor de la carta
+    const valueCard = () => {
+
+    };
+
+    //Contador de puntaje
+    const countScore = () => {
+
+    };
+
+    //Crear carta para mostrarla en DOM
+    const createCard = () => {
+
+    };
+
+    //Determinar un ganador
+    const winnerPlayer = () => {
+
+    };
+
+    //Turno para la Computadora
+    const turnComputer = () => {
+
+    };
+
+    /* LOS EVENTOS DE LOS BOTONES */
+
+    newBtnGame.addEventListener('click', () => {
+        startGame();
+    });
+
+
+
 
 
 })();
