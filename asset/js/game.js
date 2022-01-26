22e95db7be98d045a9da59a7bf5e543f3268e322
Daniel Portales <portalesbeltrandaniel@gmail.com>
2022-01-25 21:30:59 -0300

    archivo js game

---
+++ asset/js/game.js
@@ -0,0 +1,27 @@
+/**
+ * 2C = two of Clubs (treboles)
+ * 2D = two of Diamodns (diamantes)
+ * 2H = two of Hearts (corazones)
+ * 2S = two of Spades (espadas)
+ * 
+ * La meta es llegar a 21 pts sin pasarnos
+ */
+
+//funcion anonima auto-invovcada
+(() => {
+    let deckOfCards = [];
+    //C = treboles D = diamantes H = corazones S = espadas
+    const types = ['C', 'D', 'H', 'S'];
+    const specials = ['A', 'J', 'Q', 'K'];
+
+    /*--- REFERENCIAS AL DOM  ---*/
+
+    //botones de acciones
+    const getBtnCard = document.querySelector("#btnGetCard");
+    const stopBtnTurn = document.querySelector("#btnStopTurn");
+    const newBtnGame = document.querySelector("#btnNewGame");
+
+    //referencias al area del juego
+
+
+})();
